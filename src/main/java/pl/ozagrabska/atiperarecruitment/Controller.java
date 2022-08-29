803e67831c0dd8e6d7d6cf0cfad93a3be3f0727a
Aleksandra Zagrabska <ozagrabska@gmail.com>
2022-08-29 13:14:24 +0200

    Add GET Mapping "show"
    Add Mapping that fetch content of "cat-fact".

--- src/main/java/pl/ozagrabska/atiperarecruitment/Controller.java
+++ src/main/java/pl/ozagrabska/atiperarecruitment/Controller.java
@@ -2,6 +2,8 @@ package pl.ozagrabska.atiperarecruitment;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import java.util.*;
 
 @RestController
 public class Controller {
@@ -9,5 +11,15 @@ public class Controller {
     @GetMapping("/hello")
     public String helloWorld(){
         return "Hello from Controller!";
+    }
+
+    @GetMapping("/show")
+    public List<Object> getCountries() {
+        String url = "https://cat-fact.herokuapp.com/facts";
+        RestTemplate restTemplate = new RestTemplate();
+
+        Object[] countries = restTemplate.getForObject(url, Object[].class);
+
+        return Arrays.asList(countries);
     }
 } 
